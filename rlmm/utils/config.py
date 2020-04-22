b50bcefae13679ef6a70acc85f86086c2ea1fb05
David Huang <davidxhuang@uchicago.edu>
2020-04-22 14:56:42 -0500

    Added a config class skeleton, proposal

--- rlmm/utils/config.py
+++ rlmm/utils/config.py
@@ -1,8 +1,49 @@
+import yaml
 
-
-class Config:
-    def __init__(self):
+# Configurable sub-class
+class Configurable_1:
+    def __init__(self, options):
+        print(options)
         pass
+
+# Configuable sub-class
+class Configurable_2:
+    def __init__(self, options):
+        print(options)
+        pass
+
+# Configurable container master-class
+class Config:
+
+    # Configurable sub-class constructor look-up
+    configurable = {
+        'Configurable_1': Configurable_1,
+        'Configurable_2': Configurable_2
+    }
+
+    # Calls upon the configurable sub-class constructors
+    # Create a hash-table of configurable sub-class objects
+    def __init__(self, config_dict):
+        self.configs = {}
+        for k, v in config_dict.items():
+            self.configs[k] = self.configurable[k](v)
+        pass
+
+    # Load from yaml, alternative constructor
+    @classmethod
+    def load_yaml(cls, file):
+        with open(file, 'r') as f:
+            cls(yaml.load(f, Loader=yaml.FullLoader))
+
+    # Dump to yaml... maybe to be re-tooled into an 'accumulator' of sub-class dumps
+    def dump_yaml(self, filepath):
+        with open(filepath, 'w') as f:
+            yaml.dump(f)
+
+
+if __name__ == "__main__":
+    Config.load_yaml('test.config.yaml')
+
 
 class Configurable:
     def __init__(self, config_ : Config):
