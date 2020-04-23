431d2feecfdaa6a4d9e16f9c6cc2948848faef92
Austin Clyde <aclyde11@gmail.com>
2020-04-23 17:43:30 -0500

    Add open mm example code (#2)
    * added some basic examples and templates
    
    * updated env
    
    * added boilerplate code
    
    * added boilerplate code
    
    * Added a config class skeleton, proposal
    
    * added boilerplate code
    
    * added gym to try to fix travis.
    
    * retooled config
    
    * updated yaml
    
    * edited depencies again
    
    Co-authored-by: David Huang <davidxhuang@uchicago.edu>

---
+++ rlmm/environment/openmmEnv.py
@@ -0,0 +1,95 @@
+import random
+
+import gym
+import numpy as np
+from gym import spaces
+
+from rlmm.utils.config import Config
+
+
+class OpenMMEnv(gym.Env):
+    """Custom Environment that follows gym interface"""
+    metadata = {'render.modes': ['human']}
+
+    class Config(Config):
+        def __init__(self, configs):
+            self.__dict__.update(configs)
+
+    def __init__(self, config_: Config):
+        """
+
+        :param systemloader:
+        """
+        gym.Env.__init__(self)
+        self.config = config_
+
+        self.systemloader = self.config.systemloader.get_obj()
+
+        self.obs_processor = self.config.obsmethods.get_obj()
+
+        self.action = self.config.actions.get_obj()
+        self.action_space = self.action.get_gym_space()
+        self.observation_space = self.setup_observation_space()
+
+        self.reset()
+
+    def setup_action_space(self):
+        """
+
+        :return:
+        """
+        return spaces.Discrete(2)
+
+    def setup_observation_space(self):
+        """
+
+        :return:
+        """
+        return spaces.Box(low=0, high=255, shape=(16, 16, 3), dtype=np.uint8)
+
+    def get_obs(self):
+        """
+
+        :return:
+        """
+        coords = self.openmm_simulation.get_coordinates()
+        out = self.obs_processor(coords)
+        return out
+
+    def step(self, *action):
+        """
+
+        :param action:
+        :return:
+        """
+        self.action.apply_action_simulation(action, self.openmm_simulation)
+        self.openmm_simulation.run(1)
+
+        obs = self.get_obs()
+
+        return obs, \
+               random.random(), \
+               random.random(), \
+               {}
+
+    def reset(self):
+        """
+
+        :return:
+        """
+        self.openmm_simulation = self.config.openmmWrapper.get_obj(self.systemloader)
+        return self.get_obs()
+
+    def render(self, mode='human', close=False):
+        """
+
+        :param mode:
+        :param close:
+        """
+        pass
+
+    def close(self):
+        """
+
+        """
+        pass
---
+++ rlmm/tests/test_env.py
@@ -0,0 +1,25 @@
+"""
+Unit and regression test for the rlmm package.
+"""
+
+import rlmm.environment.openmmEnv
+# Import package, test suite, and other packages as needed
+import rlmm.environment.openmmWrapper
+import rlmm.environment.systemloader
+
+from rlmm.environment.openmmEnv import OpenMMEnv
+from rlmm.utils.config import Config
+
+def test_PDBLoader_get_mobile():
+    config = Config.load_yaml('rlmm/tests/test_config.yaml')
+    env = OpenMMEnv(OpenMMEnv.Config(config.configs))
+    assert(644 == env.systemloader.get_mobile())
+
+def test_load_test_system():
+    config = Config.load_yaml('rlmm/tests/test_config.yaml')
+    env = OpenMMEnv(OpenMMEnv.Config(config.configs))
+    assert (isinstance(env.step(0.2, 0.2, 0.2), tuple))
+
+
+if __name__ == '__main__':
+    test_PDBLoader_get_mobile()
