8c9bfbf4a98db788b7c652394271fca4a804e4f8
marius0725 <49179555+marius0725@users.noreply.github.com>
2020-05-10 12:24:41 -0500

    '-Generalized sending and receiving to allow for object transfer via pickling -Improved sending and receiving methods for robustness (ie data loss and files > 4096 bytes -Added a few print lines for logging/troubleshooting
    Outstanding: Testing and Unit Tests'

--- tcp_env_wrapper.py
+++ tcp_env_wrapper.py
@@ -1,4 +1,6 @@
 import socket
+import pickle
+import struct
 import time
 import threading
 
@@ -11,6 +13,33 @@ class MockRLMMEnv:
         return "observations for action \'{}\'".format(action)
 
 
+def recvall(sock, n):
+    """Helper function to recv n bytes or return None if EOF is hit"""
+    data = bytearray()
+    while len(data) < n:
+        packet = sock.recv(n - len(data))
+        if not packet:
+            return None
+        data.extend(packet)
+    return data
+
+
+def send_msg(sock, msg):
+    """Prefix each message with a 4-byte length (network byte order)"""
+    msg = struct.pack('>I', len(msg)) + msg
+    sock.sendall(msg)
+
+
+def recv_msg(sock):
+    # Read message length and unpack it into an integer
+    raw_msglen = recvall(sock, 4)
+    if not raw_msglen:
+        return None
+    msglen = struct.unpack('>I', raw_msglen)[0]
+    # Read the message data
+    return recvall(sock, msglen)
+
+
 class TcpEnvWrapper:
     def __init__(self, env, host, port):
         self.env = env
@@ -20,13 +49,11 @@ class TcpEnvWrapper:
         self.server_thread.start()
         self.client = None
 
-
     def stop(self):
         """stop everything"""
         self.client.send("break")
         self.client.close()
         print("server and client shut off")
-
 
     def server_worker(self):
         """server that will pass commands that it receives to actual env object"""
@@ -38,25 +65,33 @@ class TcpEnvWrapper:
         while keeprunning:
             conn, addr = serv.accept()
             while True:
-                received_action = conn.recv(4096)
+                msg = recv_msg(serv)
+                received_action = pickle.loads(msg)
                 if not received_action: break
                 if received_action == 'break':
                     keeprunning = False
                     break
                 print("server received action: {}".format(received_action))
-                conn.send(self.env.act(received_action))
+                msg = pickle.dumps(self.env.act(received_action))
+                send_msg(serv, msg)
+                print('Observation sent to client')
             conn.close()
             print('client disconnected')
         print("server closed")
-
 
     def act(self, action):
         """send action to the env server tahts listening on self.host:self.port"""
         if not self.client:
             self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client.connect((self.host, self.port))
-        self.client.send(str(action))
-        obs = self.client.recv(4096)
+        # Pickle the object and send it to the server
+        msg = pickle.dumps(action)
+        send_msg(self.client, msg)
+        print('Data sent to server')
+        # Receive object and unpickle it
+        msg = recv_msg(self.client)
+        obs = pickle.loads(msg)
+        print('Data received from server')
         return obs
 
 
