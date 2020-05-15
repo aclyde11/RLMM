bdd20456cc97fc36d66080f78e91feabb9c97491
Dallon Asnes <dasnes@uchicago.edu>
2020-05-14 23:19:07 -0500

    more typehint

--- rlmm/utils/loggers.py
+++ rlmm/utils/loggers.py
@@ -1,10 +1,10 @@
 import sys
 
-def make_message_writer(verbose, class_name_):
+def make_message_writer(verbose: bool, class_name_: str):
     class MessageWriter(object):
         class_name = class_name_
 
-        def __init__(self, method_name):
+        def __init__(self, method_name: str) -> None:
             self.verbose = verbose
             self.method_name = method_name
 
@@ -15,13 +15,13 @@ def make_message_writer(verbose, class_name_):
         def error(self, *args, **kwargs):
             print("ERROR [{}:{}]".format(self.class_name, self.method_name), *args, **kwargs, file=sys.stderr)
 
-        def failure(self, *args, exit_all=False, **kwargs):
+        def failure(self, *args, exit_all: Optional[bool] = False, **kwargs):
             print("FAILURE [{}:{}]".format(self.class_name, self.method_name), *args, **kwargs, file=sys.stderr)
             if exit_all:
                 exit()
 
         @classmethod
-        def static_failure(cls, method_name, *args, exit_all=False, **kwargs):
+        def static_failure(cls, method_name: str, *args, exit_all: Optional[bool] = False, **kwargs):
             print("FAILURE [{}:{}]".format(cls.class_name, method_name), *args, **kwargs, file=sys.stderr)
             if exit_all:
                 exit()
