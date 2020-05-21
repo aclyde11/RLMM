0142f0478753311ff295c342b1d618b9c0597d8f
Austin Clyde <aclyde@uchicago.edu>
2020-05-21 03:06:20 -0500

    updated examples

--- rlmm/environment/openmmWrapper.py
+++ rlmm/environment/openmmWrapper.py
@@ -353,8 +353,7 @@ class MCMCOpenMMSimulationWrapper:
         updates = 10
         delta = int(step_size / updates)
         reporter = StateDataReporter(sys.stdout, delta, step=True, time=True, potentialEnergy=True,
-                                     kineticEnergy=True, totalEnergy=True, temperature=True, volume=True,
-                                     density=True,
+                                     kineticEnergy=True, totalEnergy=True, temperature=True,
                                      progress=True, remainingTime=True, speed=True, elapsedTime=True, separator='\t',
                                      totalSteps=step_size)
         _trajectory = np.zeros((updates, self.system.getNumParticles(), 3))
